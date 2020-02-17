8f8f0bcedcadbe3f5fc8f20093b28aa0d568f1be
Nicolas <37118153+Nickeroro@users.noreply.github.com>
2020-02-17 14:49:08 +0000

    170220-1450

--- Python_files/MeanShift.py
+++ Python_files/MeanShift.py
@@ -3,10 +3,19 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+# Default resolutions of the frame are obtained.The default resolutions are system dependent.
+# We convert the resolutions from float to integer.
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                      10, (frame_width, frame_height))
+
 # take first frame of the video
 ret,frame = cap.read()
 
-# setup initial location of window
+# setup initial location of windowa
 # r,h,c,w - region of image
 #           simply hardcoded the values
 r,h,c,w = 200,100,300,100
@@ -22,9 +31,8 @@ cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
-while(1):
+while(True):
     ret ,frame = cap.read()
-
     if ret == True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
@@ -37,14 +45,16 @@ while(1):
         img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
         cv2.imshow('img2',img2)
 
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            break
-        else:
-            cv2.imwrite(chr(k)+".jpg",img2)
+        # Write the frame into the file 'output.avi'
+        out.write(frame)
 
+        k = cv2.waitKey(60) & 0xff
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     else:
         break
 
-cv2.destroyAllWindows()
+
+out.release()
 cap.release()
+cv2.destroyAllWindows()
